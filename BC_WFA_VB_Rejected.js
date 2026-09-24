cfc09b1af01df0884827b8f48e420624c86b7bdd
JS0801 <jsuthar@oncloudconsulting.ca>
2026-09-24 10:34:23 -0400

    Update from NetSuite (Auto)

---
+++ BC_WFA_VB_Rejected.js
@@ -0,0 +1,68 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType WorkflowActionScript
+ */
+define(['N/record', 'N/log', 'N/email'], function (record, log, email) {
+    function onAction(context) {
+        try {
+            var vbId = context.newRecord.id;
+            var vbRec = record.load({
+                type: record.Type.VENDOR_BILL,
+                id: vbId,
+                isDynamic: false
+            });
+
+            var sublists = ['item', 'expense'];
+            var linesUpdated = 0;
+
+            sublists.forEach(function (sublistId) {
+                var lineCount = vbRec.getLineCount({ sublistId: sublistId });
+
+                for (var i = 0; i < lineCount; i++) {
+                    var status = vbRec.getSublistValue({
+                        sublistId: sublistId,
+                        fieldId: 'custcol_bc_approval_status',
+                        line: i
+                    });
+
+                    if (status != '3') {
+                        vbRec.setSublistValue({
+                            sublistId: sublistId,
+                            fieldId: 'custcol_bc_approval_status',
+                            line: i,
+                            value: '3'
+                        });
+                        linesUpdated++;
+                    }
+                }
+            });
+
+            vbRec.setValue({ fieldId: 'custbody_bc_vb_all_rejected', value: true });
+            vbRec.setValue({ fieldId: 'approvalstatus', value: 3 });
+            vbRec.setValue({ fieldId: 'custbody_bc_vb_wf_state', value: 5 });
+            var creator = vbRec.getValue({fieldId: 'custbody_bc_created_by'})
+            var vbTranId = vbRec.getValue({fieldId: 'tranid'})
+            try {
+                var vbRecId = vbRec.save({ignoreMandatoryFields: true});
+                if(creator){
+                    email.send({
+                        //author: authorId,
+                        author: 7173, //Athena Santiago
+                        recipients: creator,
+                        subject: 'Vendor Bill has been rejected',
+                        body: 'Vendor Bill:  ' + vbTranId + ' has been rejected.'
+                    });
+                }
+
+            }catch (e) {
+                log.debug('Error in saving approved lines', e)
+            }
+
+            log.debug('WFA: Approval finalized', linesUpdated);
+        } catch (e) {
+            log.debug('WFA error', e.message);
+        }
+    }
+
+    return { onAction };
+});
