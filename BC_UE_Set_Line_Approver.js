a0f52edd8cc8fa7c392cf374cca1934bd4ca6ec3
JS0801 <jsuthar@oncloudconsulting.ca>
2026-09-24 14:42:35 -0400

    Update from NetSuite (Auto)

--- BC_UE_Set_Line_Approver.js
+++ BC_UE_Set_Line_Approver.js
@@ -291,9 +291,9 @@ define(['N/record', 'N/runtime', 'N/url'], function (record, runtime, url) {
             label: isExpenseReport ? 'Expense Report' : 'Vendor Bill',
             sublists: isExpenseReport ? ['expense'] : ['item', 'expense'],
             stateField: isExpenseReport ? 'custbody_bc_er_wf_state' : 'custbody_bc_vb_wf_state',
-            allApprovedField: isExpenseReport ? 'custbody_bc_er_all_approved' : 'custbody_bc_vb_all_approved',
-            allRejectedField: isExpenseReport ? 'custbody_bc_er_all_rejected' : 'custbody_bc_vb_all_rejected',
-            allNoProjectField: isExpenseReport ? 'custbody_bc_er_all_no_project' : 'custbody_bc_all_no_project',
+            allApprovedField: 'custbody_bc_vb_all_approved',
+            allRejectedField: 'custbody_bc_vb_all_rejected',
+            allNoProjectField: 'custbody_bc_all_no_project',
             expenseAccountField: isExpenseReport ? 'expenseaccount' : 'account'
         };
     }
